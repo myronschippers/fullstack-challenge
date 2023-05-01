f003b01c502218f90bec65f05b14173e3e253865
Myron R Schippers Jr <dubtwee@gmail.com>
2023-05-01 00:18:28 -0400

    correcting table creation and adding a single insert for initial customer

--- database/postgres/docker-entrypoint-initdb/02_init.sql
+++ database/postgres/docker-entrypoint-initdb/02_init.sql
@@ -1,19 +1,18 @@
 -- DATABASE initialization of table structure
 
-CREATE TABLE "customers" (
-  "id" UUID NOT NULL PRIMARY KEY,
+CREATE TABLE customers (
+  "id" UUID NOT NULL PRIMARY KEY UNIQUE,
   "firstName" VARCHAR(150) NOT NULL,
   "lastName" VARCHAR(150) NOT NULL,
   "createdAt" TIMESTAMP NOT NULL,
-  "updatedAt" TIMESTAMP NOT NULL,
-  UNIQUE(id)
+  "updatedAt" TIMESTAMP NOT NULL
 );
 
 -- ADDITIONAL TABLES:
 -- products
 -- protection_plans
-CREATE TABLE "purchases" (
-  "id" UUID NOT NULL PRIMARY KEY,
+CREATE TABLE purchases (
+  "id" UUID NOT NULL PRIMARY KEY UNIQUE,
   "invoice" VARCHAR(20),
   "totalSaleAmount" MONEY NOT NULL,
   "invoiceDate" TIMESTAMP NOT NULL,
@@ -28,24 +27,22 @@ CREATE TABLE "purchases" (
   "protectionPlanPeriod" VARCHAR(10),
   "customerId" UUID REFERENCES customers (id) NOT NULL,
   "createdAt" TIMESTAMP NOT NULL,
-  "updatedAt" TIMESTAMP NOT NULL,
-  UNIQUE(id)
+  "updatedAt" TIMESTAMP NOT NULL
 );
 
 -- ADDITIONAL TABLES:
 -- status_options
 -- solution_options
 -- product_condition_codes
-CREATE TABLE "claims" (
-  "id" UUID NOT NULL PRIMARY KEY,
+CREATE TABLE claims (
+  "id" UUID NOT NULL PRIMARY KEY UNIQUE,
   "status" VARCHAR(50) NOT NULL,
   "solution" VARCHAR(50) NOT NULL,
   "createdAt" TIMESTAMP NOT NULL,
   "updatedAt" TIMESTAMP NOT NULL,
   "customerId" UUID REFERENCES customers (id) NOT NULL,
-  "productPurchaseId" UUID REFERENCES purchases (id) NOT NULL,
+  "productPurchaseId" UUID REFERENCES purchases (id) NOT NULL UNIQUE,
   "productCondition" VARCHAR(2) NOT NULL,
   "damageDescription" TEXT,
-  "damageDate" DATE NOT NULL,
-  UNIQUE(id, productPurchaseId)
+  "damageDate" DATE NOT NULL
 );
---
+++ database/postgres/docker-entrypoint-initdb/03_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO customers(id, firstName, lastName, createdAt, updatedAt)
+VALUES (
+  'ff6b9889-fbf2-40e6-b856-17948623cd78',
+  'Son',
+  'WuKong',
+  '2023-03-30 21:49:39.543653',
+  '2023-03-30 21:49:39.543653'
+);
