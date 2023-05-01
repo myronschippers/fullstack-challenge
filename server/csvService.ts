cbb974b6ed568c96808af99a84f85c49eea28420
Myron R Schippers Jr <dubtwee@gmail.com>
2023-05-01 00:44:21 -0400

    refactoring the file read stream to use a promise so it can work with async/await

--- server/csvService.ts
+++ server/csvService.ts
@@ -1,15 +1,28 @@
 import { createReadStream } from 'streamifier';
 import { parse } from 'csv';
 
-const parser = parse({ columns: true }, function (err, records) {
-  console.log(records);
-});
+const csvReadStreamPromise = (csvFile: Express.Multer.File) => {
+  return new Promise((resolve, reject) => {
+    // solution for fs.createReadStream not being able to read multer buffer
+    // https://dev.to/petrussola/upload-csv-parse-rows-and-save-each-row-to-mongodb-in-an-express-server-5738
+    createReadStream(csvFile.buffer)
+      .pipe(parse({ columns: true }))
+      .on('error', (err) => {
+        reject(err);
+      })
+      .on('data', (rowData) => {
+        console.log(rowData);
+      })
+      .on('end', () => {
+        resolve({ message: 'File Loaded Successfully' });
+      });
+  });
+};
 
 const parseCustomerCsv = async (customersCsvFile: Express.Multer.File) => {
   try {
-    // solution for fs.createReadStream not being able to read multer buffer
-    // https://dev.to/petrussola/upload-csv-parse-rows-and-save-each-row-to-mongodb-in-an-express-server-5738
-    createReadStream(customersCsvFile.buffer).pipe(parser);
+    const parseResponse = await csvReadStreamPromise(customersCsvFile);
+    return parseResponse;
   } catch (err) {
     console.log('parseCustomerCsv ERROR::', err);
     throw new Error('Error parsing Customers CSV File.');
